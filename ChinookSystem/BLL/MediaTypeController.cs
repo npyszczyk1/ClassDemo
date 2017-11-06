1617842e4913ff8808bc0a7653a6269a4fd543ec
npyszczyk1 <n.pyszczyk@gmail.com>
2017-11-06 14:45:01 -0700

    Added methods to controller classes from the UX starter kit

--- ChinookSystem/BLL/MediaTypeController.cs
+++ ChinookSystem/BLL/MediaTypeController.cs
@@ -3,36 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+#region Additional Namespaces
 using Chinook.Data.Entities;
 using Chinook.Data.POCOs;
 using ChinookSystem.DAL;
 using System.ComponentModel;
+#endregion
 
 namespace ChinookSystem.BLL
 {
     [DataObject]
-    public class ArtistController
+    public class MediaTypeController
     {
-        [DataObjectMethod(DataObjectMethodType.Select,false)]
-
-        public List<Artist> Artists_List()
-        {
-            using (var context = new ChinookContext())
-            {
-                return context.Artists.ToList();
-            }
-        }
-
         [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public List<SelectionList> List_ArtistNames()
+        public List<SelectionList> List_MediaTypeNames()
         {
             using (var context = new ChinookContext())
             {
-                var results = from x in context.Artists
+                var results = from x in context.MediaTypes
                               orderby x.Name
                               select new SelectionList
                               {
-                                  IDValueField = x.ArtistId,
+                                  IDValueField = x.MediaTypeId,
                                   DisplayText = x.Name
                               };
                 return results.ToList();
